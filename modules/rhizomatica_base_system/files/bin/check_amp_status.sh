7c65394a372177d94302416c2064acb28fc40310
Ciaby <ciaby@rhizomatica.org>
2015-02-16 14:24:42 -0600

    Still more refactoring on monitor code

--- modules/rhizomatica_base_system/files/bin/check_amp_status.sh
+++ modules/rhizomatica_base_system/files/bin/check_amp_status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Check for AMP status on each BTS, from BTS1 up to BTS10
+# Check for AMP status on each BTS, from BTS1 up to BTS3
 . ./vars.sh
-for bts in $BTS1 $BTS2 $BTS3 $BTS4 $BTS5 $BTS6 $BTS7 $BTS7 $BTS8 $BTS9 $BTS10; do
+for bts in $BTS1 $BTS2 $BTS3; do
     echo "BTS $bts:";
     ssh root@$bts sbts2050-util sbts2050-pwr-status | grep Amp;
 done
--- modules/rhizomatica_base_system/files/bin/turn_on_amplifier.sh
+++ modules/rhizomatica_base_system/files/bin/turn_on_amplifier.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Turn on AMP on all BTS, from BTS1 to BTS 10
+# Turn on AMP on all BTS, from BTS1 to BTS3
 . ./vars.sh
 LOGFILE="/var/log/rhizomatica/monitor_amp.log"
 
-for bts in $BTS1 $BTS2 $BTS3 $BTS4 $BTS5 $BTS6 $BTS7 $BTS7 $BTS8 $BTS9 $BTS10; do
-    logc "Turning on AMP on BTS $bts:"
+for bts in $BTS1 $BTS2 $BTS3; do
+    logc "Turning on AMP on BTS $bts:";
     ssh root@$bts "sbts2050-util sbts2050-pwr-enable 1 1 1";
 done
