912a2862782acbc17a248119cbf44887a8019b1c
Ciaby <ciaby@rhizomatica.org>
2015-05-29 14:59:00 -0500

    Customize Icinga2 configuration Add 30 seconds timeout to the apt check

--- modules/rhizo_base/manifests/icinga.pp
+++ modules/rhizo_base/manifests/icinga.pp
@@ -12,9 +12,16 @@
 #
 class rhizo_base::icinga {
 
-  package { 'icinga2':
+  package { ['icinga2', 'icinga2-bin', 'icinga2-common', 'icinga2-doc']:
       ensure  => '2.3.4~precise',
       require => Class['rhizo_base::apt'],
+    }
+
+  file { '/etc/icinga2/conf.d':
+      ensure  => directory,
+      source  => 'puppet:///modules/rhizo_base/etc/icinga2/conf.d',
+      recurse => remote,
+      require => Package['icinga2'],
     }
 
   }
