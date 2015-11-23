85a567e3aa41f48c4e1d7daa6b3ff2e0b60217f2
Ciaby <ciaby@rhizomatica.org>
2015-11-23 19:02:04 +0100

    Change RCCN version to 1.0.7 (localnet)

--- modules/rhizo_base/manifests/init.pp
+++ modules/rhizo_base/manifests/init.pp
@@ -189,7 +189,7 @@ class rhizo_base {
       ensure   => present,
       provider => git,
       source   => 'https://github.com/Rhizomatica/rccn.git',
-     revision => '1.0.6',
+     revision => '1.0.7',
       require  => [ File['/var/rhizomatica'], Package['git'] ],
       notify   => [ Exec['locale-gen'],
                     Exec['restart-freeswitch'],
