4616dfa0a77da734aa897db484684d04fe5b711c
Keith <keith@rhizomatica.org>
2016-04-19 21:32:11 +0100

    Bump Tag

--- modules/rhizo_base/manifests/init.pp
+++ modules/rhizo_base/manifests/init.pp
@@ -190,7 +190,7 @@ class rhizo_base {
       ensure   => present,
       provider => git,
       source   => 'https://github.com/Rhizomatica/rccn.git',
-     revision => '1.0.81',
+     revision => '1.0.82',
       require  => [ File['/var/rhizomatica'], Package['git'] ],
       notify   => [ Exec['locale-gen'],
                     Exec['restart-freeswitch'],
