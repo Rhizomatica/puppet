404a0179a63676a00000884e72de66ec280ae3b7
Keith <keith@rhizomatica.org>
2016-05-26 17:29:55 +0100

    Tag

--- modules/rhizo_base/manifests/init.pp
+++ modules/rhizo_base/manifests/init.pp
@@ -193,7 +193,7 @@ class rhizo_base {
       ensure   => present,
       provider => git,
       source   => 'https://github.com/Rhizomatica/rccn.git',
-      revision => '1.0.87',
+      revision => '1.0.88',
       require  => [ File['/var/rhizomatica'], Package['git'] ],
       notify   => [ Exec['locale-gen'],
                     Exec['restart-freeswitch'],
