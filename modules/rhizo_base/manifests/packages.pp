838c1e1fa9cfb7b09553b94df1afb128968eb77b
Wile E. Coyote <rhizomatica@rhizomatica.org>
2016-11-21 14:38:29 +0000

    Install apcupsd

--- modules/rhizo_base/manifests/packages.pp
+++ modules/rhizo_base/manifests/packages.pp
@@ -13,7 +13,7 @@
 class rhizo_base::packages {
 
   package { ['mosh', 'git', 'openvpn', 'lm-sensors', 'runit', 'sqlite3',
-            'libffi-dev']:
+            'libffi-dev', 'apcupsd']:
       ensure  => installed,
       require => Class['rhizo_base::apt'],
     }
