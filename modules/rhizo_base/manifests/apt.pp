4a0d62ad30a0f3b5abe2cca5ecd11700f4160271
Wile E. Coyote <rhizomatica@rhizomatica.org>
2017-01-13 22:33:57 +0000

    Add new 'repo' repo for updated osmo pkgs

--- modules/rhizo_base/manifests/apt.pp
+++ modules/rhizo_base/manifests/apt.pp
@@ -25,6 +25,13 @@ class rhizo_base::apt {
       include_src       => false,
       require           => File['/etc/apt/apt.conf.d/90unsigned'],
     }
+  apt::source { 'rhizo':
+      location          => 'http://repo.rhizomatica.org/ubuntu/',
+      release           => 'precise',
+      repos             => 'main',
+      include_src       => false,
+      require           => File['/etc/apt/apt.conf.d/90unsigned'],
+    }
   apt::source { 'icinga':
       location    => 'https://packages.icinga.org/ubuntu',
       release     => 'icinga-precise',
