fd714bf99af04a5d23b8fb4ff0cdc9a8598e3a4b
Wile E. Coyote <rhizomatica@rhizomatica.org>
2017-05-05 08:49:08 +0000

    Make logc standard in these monitor* scripts

--- modules/rhizo_base/files/bin/monitor_rapi.sh
+++ modules/rhizo_base/files/bin/monitor_rapi.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+RHIZO_SCRIPT="/home/rhizomatica/bin"
+. $RHIZO_SCRIPT/vars.sh
 
 LOGFILE="/var/log/monitor_rapi.log"
-
-function logc() {
-        txt=$1
-        echo "[`date '+%d-%m-%Y %H:%M:%S'`] $txt" >> $LOGFILE
-}
 
 curl --connect-timeout 180 --max-time 180  -X GET http://localhost:8085/configuration/site 2>/dev/null
 if [ $? -gt 0 ]; then
