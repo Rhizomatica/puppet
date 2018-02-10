9e5c09cb80449ef10637dbd0e18ac71f98086040
Wile E. Coyote <rhizomatica@rhizomatica.org>
2018-02-10 19:47:01 +0000

    Make sure LiteCel Amp is OFF

---
+++ modules/rhizo_base/files/bin/turn_off_amplifier.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Turn on AMP on all BTS, from BTS1 to BTS3
+RHIZO_SCRIPT="/home/rhizomatica/bin"
+. $RHIZO_SCRIPT/vars.sh
+LOGFILE="/var/log/monitor_amp.log"
+
+for bts in $BTS1 $BTS2 $BTS3; do
+    logc "Turning off AMP on BTS $bts:";
+    ssh root@$bts "sbts2050-util sbts2050-pwr-enable 1 1 0";
+done
+
