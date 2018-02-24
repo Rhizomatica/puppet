64931584153da9eff69b1e346a4e4a4074dda4ab
Keith Whyte <keith@rhizomatica.org>
2018-02-24 11:47:39 +0000

    Packages: Add curl, as we use it in the install. Add htop, because it's nice.

--- modules/rhizo_base/manifests/packages.pp
+++ modules/rhizo_base/manifests/packages.pp
@@ -18,7 +18,7 @@ class rhizo_base::packages::common {
 
   package { ['mosh', 'git', 'openvpn', 'lm-sensors', 'runit-systemd', 'sqlite3',
             'libffi-dev', 'apcupsd', 'expect', 'gawk', 'swig', 'g++',
-            'libcdk5' , 'sngrep', 'rrdtool', 'dnsmasq', 'joe',
+            'libcdk5' , 'sngrep', 'rrdtool', 'dnsmasq', 'joe', 'curl', 'htop',
             'python-unidecode', 'python-dateutil', 'python-yaml', 'python-formencode',
             'python-smpplib', 'python-psycopg2', 'python-pysqlite2' ]:
       ensure  => installed,
