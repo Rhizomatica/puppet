d2f3bc5757c525d11d86f1ab89222f01c28a8b13
Wile E. Coyote <rhizomatica@rhizomatica.org>
2018-11-17 09:08:33 +0000

    No not blindly restart freeswitch.
    Remove this dinosaur idea that it's OK to drop calls on everyone.

--- modules/rhizo_base/files/bin/monitor_freeswitch.sh
+++ modules/rhizo_base/files/bin/monitor_freeswitch.sh
@@ -9,12 +9,12 @@ LOGFILE="/var/log/monitor_fs.log"
 
 FS_STATUS=`fs_cli -x "sofia status"`
 
-if !(echo $FS_STATUS | grep -q "external::provider") && (ping -qc 5 8.8.8.8 > /dev/null); then
-	logc "Missing external provider! Restarting FreeSWITCH";
-	sv restart freeswitch;
-fi
+#if !(echo $FS_STATUS | grep -q "external::provider") && (ping -qc 5 8.8.8.8 > /dev/null); then
+#	logc "Missing external provider! Restarting FreeSWITCH";
+#	sv restart freeswitch;
+#fi
 
 if !(echo $FS_STATUS | grep -q "internalvpn") && (ping -qc 5 10.23.0.2); then
-	logc "Missing internal VPN! Restarting FreeSWITCH";
-	sv restart freeswitch;
+	logc "Missing internal VPN. Restarting FreeSWITCH profile";
+	fs_cli -x "sofia profile internalvpn restart"
 fi
