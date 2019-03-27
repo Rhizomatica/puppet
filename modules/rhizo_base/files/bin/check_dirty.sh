fddf95e66a84cf21e28b4bde1e59744b8bb36caf
Keith Whyte <keith@rhizomatica.org>
2019-03-27 12:53:01 +0000

    utils: restart FS with fsctl shutdown elegant

--- modules/rhizo_base/files/bin/check_dirty.sh
+++ modules/rhizo_base/files/bin/check_dirty.sh
@@ -6,14 +6,14 @@ RHIZO_SCRIPT="/home/rhizomatica/bin"
 LOGFILE="/var/log/dirty.log"
 
 if [ -a /tmp/FS-dirty -o -a /tmp/OSMO-dirty ]; then
-        waitfor0calls
         if [ -a /tmp/FS-dirty ] ; then
           logc "Freeswitch is tagged for restart due to RCCN update"
-          /usr/bin/fs_cli -x "fsctl shutdown asap"
+          /usr/bin/fs_cli -x "fsctl shutdown elegant"
           rm /tmp/FS-dirty
-          logc "freeswitch restarted"
+          logc "freeswitch signalled to restart"
         fi
         if [ -a /tmp/OSMO-dirty ] ; then
+          waitfor0calls
           logc "NITB is tagged for restart due to Puppet update"
           sv restart osmo-nitb
           rm /tmp/OSMO-dirty
