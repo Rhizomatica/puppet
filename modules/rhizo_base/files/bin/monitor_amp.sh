54be00596849dfa8ada810b373f27b98f2137a1f
Monocots <gitmaster@rhizomatica.org>
2019-06-16 11:34:48 +0000

    Revert to turning amps ON
    partially reverts 9e5c09cb80449ef10637dbd0e18ac71f98086040
    but keeping the turn_off_amplifier.sh script.

--- modules/rhizo_base/files/bin/monitor_amp.sh
+++ modules/rhizo_base/files/bin/monitor_amp.sh
@@ -3,9 +3,9 @@ RHIZO_SCRIPT="/home/rhizomatica/bin"
 . $RHIZO_SCRIPT/vars.sh
 LOGFILE="/var/log/monitor_amp.log"
 
-$RHIZO_SCRIPT/check_amp_status.sh | grep -q ON
+$RHIZO_SCRIPT/check_amp_status.sh | grep -q OFF
 if [ $? == 0 ]; then
-        logc "Amplifier is ON! Turn off amp"
-        $RHIZO_SCRIPT/turn_off_amplifier.sh >/dev/null 2>&1
+        logc "Amplifier is OFF! Turn on amp"
+        $RHIZO_SCRIPT/turn_on_amplifier.sh >/dev/null 2>&1
         logc 'Procedure completed'
 fi
