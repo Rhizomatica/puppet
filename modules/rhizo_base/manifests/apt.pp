96bf8a01741acafd1a9817799781fe3bd02c8be9
Keith Whyte <keith@rhizomatica.org>
2020-04-16 07:04:07 +0200

    apt: implement lsbdistcodename dependent repos

--- modules/rhizo_base/manifests/apt.pp
+++ modules/rhizo_base/manifests/apt.pp
@@ -12,7 +12,7 @@
 #
 
 class rhizo_base::apt {
-  contain "rhizo_base::apt::$operatingsystem"
+  contain "rhizo_base::apt::$lsbdistcodename"
 }
 
 class rhizo_base::apt::common {
@@ -52,30 +52,57 @@ class rhizo_base::apt::common {
       require           => File['/etc/apt/apt.conf.d/90unsigned'],
     }
 
+  package {'apt-transport-https':
+      ensure  => installed,
+    }
 }
 
-class rhizo_base::apt::ubuntu inherits rhizo_base::apt::common {
+class rhizo_base::apt::buster inherits rhizo_base::apt::common {
 
-  apt::ppa { 'ppa:keithw/mosh': }
-  apt::ppa { 'ppa:ondrej/php': }
-  apt::ppa { 'ppa:ondrej/apache2': }
+  apt::source { 'freeswitch':
+      location    => 'http://files.freeswitch.org/repo/deb/freeswitch-1.8/',
+      release     => 'buster',
+      repos       => 'main',
+      key         => {
+         'id'     => '20B06EE621AB150D40F6079FD76EDC7725E010CF',
+         'source' => 'https://files.freeswitch.org/repo/deb/freeswitch-1.8/key.gpg'
+       }
+    }
 
   apt::source { 'nodesource':
       location    => 'https://deb.nodesource.com/node_0.10',
-      release     => 'precise',
+      release     => 'buster',
       repos       => 'main',
       key         => {
-        'id'      => '68576280',
+        'id'      => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
         'source'  => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
+      },
+      require     => Package['apt-transport-https'],
+    }
+
+  apt::source { 'irontec':
+      location    => 'http://packages.irontec.com/debian',
+      release     => 'buster',
+      repos       => 'main',
+      key         => {
+        'id'      => '4FF7139B43073A436D8C2C4F90D20F5ED8C20040',
+        'source'  => 'http://packages.irontec.com/public.key'
+       }
+    }
+
+  apt::source { 'osmocom':
+      location    => "http://download.opensuse.org/repositories/network:/osmocom:/${osmo_repo}/Debian_10.0/",
+      release     => './',
+      repos       => '',
+      notify      => Exec['apt_update'],
+      key         => {
+        'id'      => '0080689BE757A876CB7DC26962EB1A0917280DDF',
+        'source'  => "http://download.opensuse.org/repositories/network:/osmocom:/${osmo_repo}/Debian_10.0/Release.key"
        }
     }
 }
 
-class rhizo_base::apt::debian inherits rhizo_base::apt::common {
-
-  package {'apt-transport-https':
-      ensure  => installed,
-    }
+class rhizo_base::apt::stretch inherits rhizo_base::apt::common {
 
   apt::source { 'freeswitch':
       location    => 'http://files.freeswitch.org/repo/deb/freeswitch-1.6/',
