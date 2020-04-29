e737ba29e41b6af7fe80be982a63addcd9a9aefb
Keith Whyte <keith@rhizomatica.org>
2020-04-29 05:20:08 +0200

    BTS: set nameservers seperately

--- modules/rhizo_base/files/var/SysmoBTS/provision.sh
+++ modules/rhizo_base/files/var/SysmoBTS/provision.sh
@@ -16,11 +16,12 @@ for bts in "${!BTS[@]}" ; do
   scp $SSH_OPTS udhcpc root@${BTS[$bts]}:/etc/default/udhcpc
   ssh $SSH_OPTS root@${BTS[$bts]} "echo 'root:$BTSPASS' | /usr/sbin/chpasswd"
   ssh $SSH_OPTS root@${BTS[$bts]} "date -s '$(date)'"
+  ssh $SSH_OPTS root@${BTS[$bts]} "echo nameserver 1.1.1.1 > /etc/resolv.conf; echo nameserver 9.9.9.9 >> /etc/resolv.conf"
 
   if [ "$(ssh $SSH_OPTS ${BTS[$bts]} sysmobts-util trx-nr)" == "0" ] ; then
     # Master Verified.
     echo "BTS is a 2050 Master"
-    ssh $SSH_OPTS root@${BTS[$bts]} "echo '$SITE-Master-$bts' > /etc/hostname; echo nameserver 1.1.1.1 > /etc/resolv.conf; echo nameserver 9.9.9.9 >> /etc/resolv.conf"
+    ssh $SSH_OPTS root@${BTS[$bts]} "echo '$SITE-Master-$bts' > /etc/hostname"
     scp $SSH_OPTS master/gpsd root@${BTS[$bts]}:/etc/default/gpsd
     scp $SSH_OPTS master/ntp.conf root@${BTS[$bts]}:/etc/ntp.conf
     scp $SSH_OPTS leds.sh root@${BTS[$bts]}:/etc/init.d/leds.sh
@@ -31,7 +32,7 @@ for bts in "${!BTS[@]}" ; do
   if [ "$(ssh $SSH_OPTS ${BTS[$bts]} sysmobts-util trx-nr)" == "1" ] ; then
     # Slave Verified.
     echo "BTS is a 2050 Slave"
-    ssh $SSH_OPTS root@${BTS[$bts]} "echo '$SITE-Slave-$bts' > /etc/hostname; echo nameserver 1.1.1.1 > /etc/resolv.conf; echo nameserver 9.9.9.9 >> /etc/resolv.conf"
+    ssh $SSH_OPTS root@${BTS[$bts]} "echo '$SITE-Slave-$bts' > /etc/hostname"
     scp $SSH_OPTS slave/gpsdate root@${BTS[$bts]}:/etc/default/gpsdate
   fi
 
